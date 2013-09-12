872e3725a40969ed99e896f4d3908e411b32cba3
boris <boris.musatov@gmail.com>
2013-09-12 21:00:18 +0200

    get report by id

--- src/main/java/cz/boris/logger/rest/ReportResource.java
+++ src/main/java/cz/boris/logger/rest/ReportResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -17,12 +18,21 @@ import cz.boris.logger.service.ReportService;
 @Component
 public class ReportResource {
 
+	private static final String PARAMETER_ID = "id";
+	
 	@Autowired
 	private ReportService reportService;
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Report> getAllReports() {
 		return reportService.getAllReports();
-	}	
+	}
+
+	@GET
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Report getReport(@PathParam(PARAMETER_ID) String id) {
+		return reportService.getReport(id);
+	}
 }
